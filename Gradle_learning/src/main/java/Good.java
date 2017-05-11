e491a7bff6e924d7110b9f89caed6f1feb27db7a
rico <rico.cui@coupang.com>
2017-05-11 17:39:42 +0800

    gradle file

--- Gradle_learning/src/main/java/Good.java
+++ Gradle_learning/src/main/java/Good.java
@@ -1,11 +1,12 @@
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by rico on 5/11/17.
  */
 @Slf4j
 public class Good {
-
+@Autowired
     public void test() {
         try {
             System.out.println("try-catch test");
